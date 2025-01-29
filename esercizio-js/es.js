3b6451ae3c56dedb462831df467e4a35316ccf75
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-29 12:28:46 +0100

    js: create cards with js

--- esercizio-js/es.js
+++ esercizio-js/es.js
@@ -8,7 +8,26 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
 
     data.forEach(element => {
         let {id, title, date, url} = element;
-        
+        cardList.innerHTML += 
+        `
+            <!-- card col -->
+                <div class="col-span-full md:col-span-4">
+                    <!-- card -->
+                    <div class="relative flex flex-col transition bg-white rounded-lg shadow p-4u gap-3u shadow-makara-900/40 hover:rotate-12 hover:cursor-pointer">
+                        <!-- card pin -->
+                        <img src="img/pin.svg" alt="pin" class="absolute w-14u">
+                        <!-- card img -->
+                        <figure class="overflow-hidden rounded-md aspect-square">
+                            <img src="${url}" alt="${title}" class="img-responsive">
+                        </figure>
+                        <!-- card info -->
+                        <div>
+                            <p class="text-xl italic text-makara-600">${date}</p>
+                            <p class="uppercase h2 text-makara-900">${title}</p>
+                        </div>
+                    </div>
+                </div>
+        `
     });
 })
 .catch(error => {
