12be53502c4035f04228f1f158f8cb4191a86c5b
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-29 12:18:37 +0100

    js: fetch API

--- esercizio-js/es.js
+++ esercizio-js/es.js
@@ -1,2 +1,12 @@
 // recupero container cards
 const cardList = document.getElementById("card-list");
+
+// fetch API
+fetch('https://lanciweb.github.io/demo/api/pictures/')
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
+.catch(error => {
+
+})
